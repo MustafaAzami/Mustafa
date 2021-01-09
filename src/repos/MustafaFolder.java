1fd5150d71bdd6693c8a49751b3dcb4b2e177479
mustafaazami <mustafa.ka.azami@gmail.com>
2021-01-09 16:28:21 -0500

    numbers methode added

--- src/repos/MustafaFolder.java
+++ src/repos/MustafaFolder.java
@@ -3,7 +3,18 @@ package repos;
 public class MustafaFolder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		
+		a = a+b;
+		
+		b = a-b;
+		a = a-b;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
 
 	}
 
